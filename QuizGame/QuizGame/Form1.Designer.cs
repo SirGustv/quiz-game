9fa791fe539b6e43d47654558adc7bf29669b283
Gustavo <gust.bersan@gmail.com>
2022-03-24 22:51:46 -0300

    Definindo o layout da página inicial

--- QuizGame/QuizGame/Form1.Designer.cs
+++ QuizGame/QuizGame/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace QuizGame
 {
-    partial class Form1
+    partial class FrmInit
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnInit = new System.Windows.Forms.Button();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.lblQG = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnInit
+            // 
+            this.btnInit.Location = new System.Drawing.Point(970, 624);
+            this.btnInit.Name = "btnInit";
+            this.btnInit.Size = new System.Drawing.Size(132, 56);
+            this.btnInit.TabIndex = 0;
+            this.btnInit.Text = "Iniciar";
+            this.btnInit.UseVisualStyleBackColor = true;
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(42, 624);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(132, 56);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.Text = "Sair";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // lblQG
+            // 
+            this.lblQG.Font = new System.Drawing.Font("Lucida Sans Unicode", 25.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.lblQG.Location = new System.Drawing.Point(272, 43);
+            this.lblQG.Name = "lblQG";
+            this.lblQG.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.lblQG.Size = new System.Drawing.Size(626, 83);
+            this.lblQG.TabIndex = 2;
+            this.lblQG.Text = "QUIZ GAME";
+            this.lblQG.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // FrmInit
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.SystemColors.ControlDark;
+            this.ClientSize = new System.Drawing.Size(1145, 720);
+            this.Controls.Add(this.lblQG);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.btnInit);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "FrmInit";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "FrmInit";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnInit;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Label lblQG;
     }
 }
--- QuizGame/QuizGame/Form1.cs
+++ QuizGame/QuizGame/Form1.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace QuizGame
 {
-    public partial class Form1 : Form
+    public partial class FrmInit : Form
     {
-        public Form1()
+        public FrmInit()
         {
             InitializeComponent();
         }
